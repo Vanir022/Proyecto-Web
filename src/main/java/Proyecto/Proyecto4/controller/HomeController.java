461e0ead744d1fe6f31f2b5cbd15b7610c11377a
Vanir022 <210393690+Vanir022@users.noreply.github.com>
2025-12-02 15:15:34 -0500

    Final

--- src/main/java/Proyecto/Proyecto4/controller/HomeController.java
+++ src/main/java/Proyecto/Proyecto4/controller/HomeController.java
@@ -50,8 +50,8 @@ public class HomeController {
     
     @GetMapping("/login")
     public String login(@RequestParam(value = "error", required = false) String error,
-                       @RequestParam(value = "logout", required = false) String logout,
-                       Model model) {
+                        @RequestParam(value = "logout", required = false) String logout,
+                        Model model) {
         if (error != null) {
             model.addAttribute("errorMessage", "Email o contraseña incorrectos. Por favor, inténtelo de nuevo.");
         }
